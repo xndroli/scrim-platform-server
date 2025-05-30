f758fd48a71c5028fe544b9315b4e6abcec6f702
xndroli <103154250+xndroli@users.noreply.github.com>
2025-05-30 00:14:48 -0400

    added discord social provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -109,6 +109,13 @@ export const auth = betterAuth({
     }
   },
 
+  socialProviders: {
+    discord: {
+        clientId: process.env.DISCORD_CLIENT_ID!,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET!
+    }
+},
+
   plugins: [
     // twoFactor({
     //   issuer: "Raijin Ascendancy",
